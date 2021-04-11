b8cffff2aabad5fdf66d43de620b11f60c2ecb84
Elif Turan <eelifyagmurturan@gmail.com>
2021-04-11 20:43:29 +0300

    Added hovering effect

--- project2/main.js
+++ project2/main.js
@@ -1,7 +1,8 @@
 import './style.css'
 import * as THREE from 'three';
+import * as dat from 'dat.gui';
 
-var scene, camera, light, renderer;
+var scene, camera, light, renderer, gui, mouse, raycaster;
 
 scene = new THREE.Scene();
 
@@ -19,10 +20,16 @@ document.body.appendChild(renderer.domElement);
 
 const planeGeometry = new THREE.PlaneGeometry(10,10, 10, 10);
 const planeMaterial = new THREE.MeshPhongMaterial({
-  color: 0x0000FF,
   side: THREE.DoubleSide,
-  flatShading: THREE.FlatShading})
+  flatShading: THREE.FlatShading,
+  vertexColors: true})
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
+const colors = [];
+for(let i=0; i<planeMesh.geometry.attributes.position.count; i++){
+  colors.push(1,1,0);
+}
+planeMesh.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3)
+)
 scene.add(planeMesh);
 
 // add jaggy depth effect
@@ -37,7 +44,63 @@ for(let i =0; i<array.length; i=i+3){
 function animate(){
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObject(planeMesh);
+  //hovering effect
+  if(intersects.length>0){
+    const {color} = intersects[0].object.geometry.attributes;
+    //vertice 0
+    color.setX(intersects[0].face.a, 0);
+    // vertice 1
+    color.setX(intersects[0].face.b, 0);
+    // vertice 2
+    color.setX(intersects[0].face.c, 0);
+    color.needsUpdate = true;
+  }
 }
 
+gui = new dat.GUI();
+const world = {
+  plane: {
+    width: 10,
+    height: 10,
+    widthSegments: 5,
+    heightSegments: 5
+  }
+}
+
+gui.add(world.plane, 'width', 1, 20).onChange(generatePlane);
+gui.add(world.plane, 'height', 1, 15).onChange(generatePlane);
+gui.add(world.plane, 'widthSegments', 1, 15).onChange(generatePlane);
+gui.add(world.plane, 'heightSegments', 1, 15).onChange(generatePlane);
+
+function generatePlane(){
+  planeMesh.geometry.dispose();
+  planeMesh.geometry = new THREE.PlaneGeometry(
+    world.plane.width, 
+    world.plane.height, 
+    world.plane.widthSegments, 
+    world.plane.heightSegments);
+  
+  // add jaggy depth effect
+  const {array} = planeMesh.geometry.attributes.position;
+  for(let i =0; i<array.length; i=i+3){
+    const first = array[i];
+    const second = array [i+1];
+    const third = array[i+2];
+    array[i+ 2] = third + Math.random();
+  }
+}
+
+mouse = {
+  x: undefined,
+  y: undefined
+}
+addEventListener('mousemove', (event) => {
+  mouse.x = (event.clientX/ innerWidth)*2 -1;
+  mouse.y = -(event.clientY/ innerHeight)*2 +1;
+})
+
+raycaster = new THREE.Raycaster();
 
 animate();
