0d54f2ba665b0d9d96d2691f58b528719229a56a
Elif Turan <eelifyagmurturan@gmail.com>
2021-04-13 16:02:11 +0300

    added extrudegeometry triangle

--- project3/main.js
+++ project3/main.js
@@ -1,8 +1,9 @@
 import './style.css'
 import * as THREE from 'three';
 
-var scene, camera, light, renderer;
-var geometry, material, mesh;
+var scene, camera, renderer;
+var IcosahedronGeometry, IcosahedronMaterial, IcosahedronMesh;
+var triangle, triangleGeometry, triangleMaterial, triangleMesh;
 
 function init(){
 
@@ -11,27 +12,39 @@ function init(){
   camera = new THREE.PerspectiveCamera(75, innerWidth/innerHeight, 0.1, 1000);
   camera.position.z = 10;
 
-  light =  new THREE.DirectionalLight(0xFFFFFF, 1);
-  light.position.set(0, 0, 1);
-  scene.add(light);
-
   renderer = new THREE.WebGLRenderer();
   renderer.setPixelRatio(devicePixelRatio);
   renderer.setSize(innerWidth, innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  geometry = new THREE.IcosahedronGeometry(2, 1);
-  material = new THREE.MeshNormalMaterial();
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
+  IcosahedronGeometry = new THREE.IcosahedronGeometry(2, 1);
+  IcosahedronMaterial = new THREE.MeshNormalMaterial();
+  IcosahedronMesh = new THREE.Mesh(IcosahedronGeometry, IcosahedronMaterial);
+  scene.add(IcosahedronMesh);
 
+  triangle = new THREE.Shape([
+    new THREE.Vector3(0, 3),
+    new THREE.Vector3(3, 3),
+    new THREE.Vector3(3, 0)
+  ]);
+  triangleGeometry = new THREE.ExtrudeGeometry(triangle,
+    {bevelEnabled: false,
+      depth: 30
+  });
+
+  triangleMaterial = new THREE.MeshNormalMaterial();
+  triangleMesh = new THREE.Mesh(triangleGeometry, triangleMaterial);
+  triangleMesh.rotation.x  = -5;
+  triangleMesh.rotation.y = -9;
+  scene.add(triangleMesh);
+  
 
 }
 function animate() {
   requestAnimationFrame(animate);
 
-  mesh.rotation.x = Date.now()* 0.00005;
-  mesh.rotation.y = Date.now()* 0.0001;
+  IcosahedronMesh.rotation.x = Date.now()* 0.0005;
+  IcosahedronMesh.rotation.y = Date.now()* 0.001;
 
   renderer.render(scene, camera);
 }
